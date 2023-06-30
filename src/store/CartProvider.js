4c4c8989acbb34dce5d980cb8e1483bbfd531bd8
Tung Phan <thanhtungphanvu@gmail.com>
2023-06-30 15:08:51 +0300

    add logic added

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,36 +1,43 @@
 import { useReducer } from "react";
 import CartContext from "./cart-context";
-const defaultCartState={
-    items: [],
-    totalAmount: 0,
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
 };
-const cartReducer = (state,action)=>{
-    if (action.type === 'ADD'){
-        const updatedItems = state.items.concat(action.item);
-        const updatedItemsAmount = state.totalAmount + action.item.price * action.item.amount;
-        return {
-            items: updatedItems,
-            totalAmount: updatedItemsAmount
-        }
-    }
-    
-    return defaultCartState;
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updatedItems = state.items.concat(action.item);
+    const updatedItemsAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedItemsAmount,
+    };
+  }
+
+  return defaultCartState;
 };
 const CartProvider = (props) => {
-    const [cartState, cartDispatchHandler] = useReducer(cartReducer, defaultCartState)
-    const addItemToCartHandler = (item) => {
-        cartDispatchHandler({type:'ADD', item: item})
-    };
-    const removeItemFromCartHandler = (id) => {
-        cartDispatchHandler({type:'REMOVE', id: id})
-    };
-    const cartContext = {
-        items:[],
-        totalAmount: 0,
-        addItem: addItemToCartHandler,
-        removeItems: removeItemFromCartHandler
-
-    }
-  return <CartContext.Provider value={cartContext}>{props.children}</CartContext.Provider>;
+  const [cartState, cartDispatchHandler] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+  const addItemToCartHandler = (item) => {
+    cartDispatchHandler({ type: "ADD", item: item });
+  };
+  const removeItemFromCartHandler = (id) => {
+    cartDispatchHandler({ type: "REMOVE", id: id });
+  };
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: 0,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
 };
 export default CartProvider;
